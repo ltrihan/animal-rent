c5a3800d7a029f5725cfaae7ce8c05669e66802d
Justine Muguet <justine.jackson@hotmail.fr>
2019-05-21 12:24:34 +0200

    add animals controller

---
+++ app/controllers/animals_controller.rb
@@ -0,0 +1,41 @@
+class AnimalsController < ApplicationController
+
+  def index
+    @animals = Animal.all
+  end
+
+def show
+  @animal = Animal.find(params[:id])
+end
+
+def new
+  @animal = Animal.new
+end
+
+def create
+  @animal = Animal.new(animal_params)
+  if @animal.save
+    redirect_to user_path(@user)
+  else
+    render 'new'
+end
+
+def edit
+  @animal = Animal.find(params[:id])
+end
+
+def update
+  @animal = Animal.find(params[:id])
+  @animal.update(animal_params)
+end
+
+def destroy
+  @animal = Animal.find(params[:id])
+  @animal.destroy
+  redirect_to user_path(@user)
+end
+
+def animal_params
+  params.require(:animal).permit(:name)
+
+end
